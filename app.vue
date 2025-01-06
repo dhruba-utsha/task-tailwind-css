601894a8916f4824d864b427c4519b8c44c7cd8e
dhruba-utsha <utshasen676@gmail.com>
2025-01-06 17:40:42 +0600

    first web-page added

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="bg-white pb-[50px]">
+    <header class="flex flex-col items-center">
+        <img class="h-24 w-44 mt-[50px] mb-[30px]" src="assets\images\logo.png" alt="Fast Service Logo">
+        <h1 class="text-[#1B4B36] text-[24px] font-bold text-center">স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!</h1>
+        <h1 class="text-[#1B4B36] text-[24px] font-bold text-center mt-[10px]">"আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</h1>
+        <h3 class="text-black-700 text-[18px] font-bold text-center bg-[#EBFF3D] rounded p-[10px] m-[30px]">আপনার জীবনকে আরও স্মার্ট করুন।</h3>
+    </header>
+  </section>
+
+</template>
+
