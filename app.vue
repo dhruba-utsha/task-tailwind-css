0596d0da9359f0f52571f9ec999774058e68fb92
dhruba-utsha <utshasen676@gmail.com>
2025-01-07 13:57:10 +0600

    Full Webpage Completed

--- app.vue
+++ app.vue
@@ -1,12 +1,134 @@
 <template>
+
+  <!-- Page 1-->
   <section class="bg-white pb-[50px]">
     <header class="flex flex-col items-center">
-        <img class="h-24 w-44 mt-[50px] mb-[30px]" src="assets\images\logo.png" alt="Fast Service Logo">
-        <h1 class="text-[#1B4B36] text-[24px] font-bold text-center">স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!</h1>
-        <h1 class="text-[#1B4B36] text-[24px] font-bold text-center mt-[10px]">"আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</h1>
-        <h3 class="text-black-700 text-[18px] font-bold text-center bg-[#EBFF3D] rounded p-[10px] m-[30px]">আপনার জীবনকে আরও স্মার্ট করুন।</h3>
+        <img class="lg: h-24 w-44 mt-[50px] mb-[30px] sm:" src="assets\images\logo.png" alt="Fast Service Logo">
+
+        <h1 class="text-[#1B4B36] text-[30px] font-bold text-center">স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!</h1>
+
+        <h1 class="text-[#1B4B36] text-[30px] font-bold text-center mt-[10px]">"আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</h1>
+
+        <h3 class="text-black-700 text-[16px] font-bold text-center bg-[#EBFF3D] py-[10px] px-[50px] m-[30px]">আপনার জীবনকে আরও স্মার্ট করুন।</h3>
+
+        <iframe class="h-[400px] w-[600px] mt-[40px] mb-[50px]" width="560" height="315" src="https://www.youtube.com/embed/YGQBm9Mnad8?si=cqmZ6YUiKLOdwZUi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
     </header>
+
+    <div class="flex justify-center">
+      <div class="text-[#EBFF3D] text-[18px] font-bold text-center bg-[#1B4B36] rounded-[20px] py-[10px] px-[40px] flex">
+        <h3>অর্ডার করতে ক্লিক করুন</h3>
+        <img class="w-[24px] h-[24px] ml-[10px]" src="assets\images\Vector.png" alt="icon">
+      </div>
+    </div>
   </section>
 
+  <!-- Page 2-->
+  <section class="bg-[#FAFFCE] p-[60px]">
+    <h1 class="text-[30px] font-bold ml-[40px] mb-[10px]">স্মার্টওয়াচের একাধিক ফিচার</h1>
+    <h1 class="text-[30px] font-bold ml-[40px] mb-[30px]">এক নজরে</h1>
+
+    <div>
+      <div class="grid grid-cols-3 gap-y-10 gap-x-15 place-items-center">
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300 shadow-xl">
+            <h2 class="title group-hover:text-[#EBFF3D]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h2>
+            <p class="para group-hover:text-white">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+        </div>
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300">
+            <h2 class="title group-hover:text-[#EBFF3D]">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h2>
+            <p class="para group-hover:text-white">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+        </div>
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300">
+            <h2 class="title group-hover:text-[#EBFF3D]">ফিটনেস ট্র্যাকিং</h2>
+            <p class="para group-hover:text-white">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড।
+                ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন।
+                স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+        </div>
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300">
+            <h2 class="title group-hover:text-[#EBFF3D]">স্মার্ট নোটিফিকেশন</h2>
+            <p class="para group-hover:text-white">কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে। গুরুত্বপূর্ণ আপডেট কখনোই মিস করবেন না।
+            </p>
+        </div>
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300">
+            <h2 class="title group-hover:text-[#EBFF3D]">দীর্ঘস্থায়ী ব্যাটারি লাইফ</h2>
+            <p class="para group-hover:text-white">একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা। দ্রুত চার্জিং প্রযুক্তি।</p>
+        </div>
+        <div class="group box hover:bg-[#1B4B36] ease-in duration-300">
+            <h2 class="title group-hover:text-[#EBFF3D]">মিউজিক এবং ক্যামেরা নিয়ন্ত্রণ</h2>
+            <p class="para group-hover:text-white">মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে।
+                সহজ এবং সাবলীল ব্যবহারের সুবিধা।</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="flex justify-center mt-[60px]">
+      <div class="text-[#EBFF3D] text-[18px] font-bold text-center bg-[#1B4B36] rounded-[20px] py-[10px] px-[40px] flex">
+        <h3>অর্ডার করতে ক্লিক করুন</h3>
+        <img class="w-[24px] h-[24px] ml-[10px]" src="assets\images\Vector.png" alt="icon">
+      </div>
+    </div>
+  </section>
+
+  <!-- Page 3-->
+   <section class="bg-[#ffffff] p-[60px]">
+    <h1 class="text-[30px] font-bold text-center mb-[30px]">আমাদের পণ্য নিয়ে গ্রাহকদের অভিজ্ঞতা</h1>
+
+    <div class="flex justify-center">
+      <div class="box2">
+          <h1 class="line1">সন্তুষ্ট গ্রাহক</h1>
+          <h2 class="line2">৩,০০০+</h2>
+          <p class="line3">সন্তুষ্ট গ্রাহকরা আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।</p>
+      </div>
+
+      <div class="box2">
+          <h1 class="line1">পুনঃক্রয়কারী</h1>
+          <h2 class="line2">৪০০+</h2>
+          <p class="line3">পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করে।</p>
+      </div>
+    </div>
+
+    <div class="flex justify-center">
+      <div class="box2">
+          <h1 class="line1">পণ্য বিক্রি</h1>
+          <h2 class="line2">৫০০০+</h2>
+          <p class="line3">৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তা প্রমাণ করে।</p>
+      </div>
+
+      <div class="box2">
+          <h1 class="line1">গ্রাহক সন্তুষ্ট</h1>
+          <h2 class="line2">৯৫%</h2>
+          <p class="line3">৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।</p>
+      </div>
+    </div>
+
+    <div class="flex justify-center mt-[40px]">
+      <div class="text-[#1B4B36] text-[18px] font-bold text-center bg-[#EBFF3D] rounded-[20px] py-[10px] px-[40px] flex">
+        <h3>অর্ডার করতে ক্লিক করুন</h3>
+        <img class="w-[24px] h-[24px] ml-[10px]" src="assets\images\Vector2.png" alt="icon">
+      </div>
+    </div>
+   </section>
+
+   <!-- footer-->
+  <section class="flex justify-center items-center h-[200px] bg-[#0C3200]">
+    <div class="w-[250px]">
+      <img src="assets/images/phone.png" alt="contact info">
+    </div>
+
+    <div class="w-[150px] mx-[150px]">
+      <img src="assets\images\logo2.png" alt="logo">
+    </div>
+    <div class="w-[170px]">
+      <img src="assets\images\social.png" alt="social media">
+    </div>
+  </section>
+
+  <section class="text-center h-[100px] text-[#ffffff] bg-[#0A2900]">
+      <p class="pt-[40px]">"© 2024 All Rights Reserved by BDFunnelBuilder. Unauthorized use prohibited."</p>
+  </section>
 </template>
 
